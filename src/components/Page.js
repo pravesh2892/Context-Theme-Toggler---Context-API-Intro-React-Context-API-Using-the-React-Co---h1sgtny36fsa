5123bbee7018704584dc419652cda0f06ec1c5a6
pravesh meena <pravesh.meena2892@gmail.com>
2023-09-09 10:19:43 +0530

    Update Page.js

--- src/components/Page.js
+++ src/components/Page.js
@@ -6,11 +6,20 @@ const Page = () => {
   const { theme } = useTheme();
 
   return (
-    <div className={`container ${theme === 'light' ? 'bg-light' : 'bg-dark'}`} id="themed-page">
-      <p id="themed-text-container" className={theme === 'light' ? 'txt-light' : 'txt-dark'}>
+    <div
+      className={`container ${theme === 'light' ? 'bg-light' : 'bg-dark'}`}
+      id="themed-page"
+    >
+      <p
+        id="themed-text-container"
+        className={theme === 'light' ? 'txt-light' : 'txt-dark'}
+      >
         Lorem ipsum dolor sit amet.
       </p>
-      <button className={`btn ${theme === 'light' ? 'btn-light txt-light' : 'btn-dark txt-dark'}`} id="themed-button">
+      <button
+        className={`btn ${theme === 'light' ? 'btn-light txt-light' : 'btn-dark txt-dark'}`}
+        id="themed-button"
+      >
         Themed Button
       </button>
       <LocalThemedBox />
